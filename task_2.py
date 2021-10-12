6db2f07ea36f6f79c09e38fb73be1de5f0f98700
Olga <83516917+Hellica-Ordo@users.noreply.github.com>
2021-10-12 18:56:46 +0300

    Алгоритмы от 28.09.21, Ольга Тимофеева, ДЗ 4

---
+++ task_2.py
@@ -0,0 +1,88 @@
+"""
+Задание 2.
+
+Приведен код, который формирует из введенного числа
+обратное по порядку входящих в него цифр.
+Задача решена через рекурсию
+Выполнена попытка оптимизировать решение через мемоизацию.
+Сделаны замеры обеих реализаций.
+
+Сделайте аналитику, нужна ли здесь мемоизация или нет и почему?!!!
+Будьте внимательны, задание хитрое. Не все так просто, как кажется.
+"""
+
+from timeit import timeit
+from random import randint
+
+
+def recursive_reverse(number):
+    if number == 0:
+        return ''
+    return f'{str(number % 10)}{recursive_reverse(number // 10)}'
+
+
+num_100 = randint(10000, 1000000)
+num_1000 = randint(1000000, 10000000)
+num_10000 = randint(100000000, 10000000000000)
+
+print('Неоптимизированная функция recursive_reverse')
+print(
+    timeit(
+        "recursive_reverse(num_100)",
+        setup='from __main__ import recursive_reverse, num_100',
+        number=10000))
+print(
+    timeit(
+        "recursive_reverse(num_1000)",
+        setup='from __main__ import recursive_reverse, num_1000',
+        number=10000))
+print(
+    timeit(
+        "recursive_reverse(num_10000)",
+        setup='from __main__ import recursive_reverse, num_10000',
+        number=10000))
+
+
+def memoize(f):
+    cache = {}
+
+    def decorate(*args):
+
+        if args in cache:
+            return cache[args]
+        else:
+            cache[args] = f(*args)
+            return cache[args]
+    return decorate
+
+
+@memoize
+def recursive_reverse_mem(number):
+    if number == 0:
+        return ''
+    return f'{str(number % 10)}{recursive_reverse_mem(number // 10)}'
+
+
+print('Оптимизированная функция recursive_reverse_mem')
+print(
+    timeit(
+        'recursive_reverse_mem(num_100)',
+        setup='from __main__ import recursive_reverse_mem, num_100',
+        number=10000))
+print(
+    timeit(
+        'recursive_reverse_mem(num_1000)',
+        setup='from __main__ import recursive_reverse_mem, num_1000',
+        number=10000))
+print(
+    timeit(
+        'recursive_reverse_mem(num_10000)',
+        setup='from __main__ import recursive_reverse_mem, num_10000',
+        number=10000))
+
+"""
+ВЫВОДЫ:
+Мемоизация через декоратор не нужна в данном случае. Функция recursive_reverse_mem в примере с "оптимизацией" отрабатывает полноценно лишь в первый раз, 
+во всех остальных итерациях декоратор возвращает значение из кэша (этим и объясняются результаты замеров). В "неоптимизированном" варианте функция
+полноценно отрабатывает нужное количество раз. Мемоизация хороша в тех случаях, когда складываемые в кэш значения изменяются (например, в рекурсивных функциях),
+но в данном случае этого не происходит."""
